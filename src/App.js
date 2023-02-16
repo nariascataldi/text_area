c6ee703052809ad3518da8313409fddf7b3ff169
nariascataldi <98494610+nariascataldi@users.noreply.github.com>
2023-02-16 17:50:41 -0300

    refactor code and style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import "./App.css";
 import { useState } from "react";
+import "./App.css";
 
 export const App = () => {
   const [highlightText, setHighlightText] = useState(
@@ -7,25 +7,13 @@ export const App = () => {
   );
 
   const MAX_LENGTH = 250;
-
-  const [isOverLimit, setIsOverLimit] = useState(false);
+  let isOverLimit = highlightText.length > MAX_LENGTH;
 
   const handleChange = (event) => {
-    setHighlightText(event.target.value);
-    setIsOverLimit(event.target.value.length > MAX_LENGTH);
-  };
-  const renderTextWithHighlight = () => {
-    if (isOverLimit) {
-      return (
-        <>
-          <span>{highlightText.slice(0, MAX_LENGTH)}<span style={{
-            backgroundColor: "rgba(242, 208, 215, 1)", color: 'rgba(217, 108, 137, 1)'
-          }}>{highlightText.slice(MAX_LENGTH)}</span></span>
-        </>
-      );
-    } else {
-      return <span>{highlightText}</span>;
-    }
+    const text = event.target.value;
+    const trimmedText = text.replace(/\s\s+/g, " "); // Reemplazar múltiples espacios por un solo espacio
+    isOverLimit = trimmedText.length > MAX_LENGTH;
+    setHighlightText(trimmedText);
   };
 
   return (
@@ -37,14 +25,27 @@ export const App = () => {
             <h4>ℹ️</h4>
           </div>
           <div className="Contador">
-            {isOverLimit ? (
-              <h4 style={{ color: "#D96C89" }}>{highlightText.length}/{MAX_LENGTH}</h4>
-            ) : (
-              <h4 style={{ color: "#212126" }}>{highlightText.length}/{MAX_LENGTH}</h4>
-            )}
+            <h4 style={{ color: isOverLimit ? "#D96C89" : "#212126" }}>
+              {highlightText.length}/{MAX_LENGTH}
+            </h4>
           </div>
         </div>
-        <div className="BackBody" style={{ border: isOverLimit && '1px solid #D96C89' }}> {renderTextWithHighlight()} </div>
+        <div className="BackBody" style={{ border: isOverLimit ? '1px solid #D96C89' : '0.1px solid #212126' }}>
+          <span>
+            {highlightText.slice(0, MAX_LENGTH)}
+            {isOverLimit && (
+              <span
+                style={{
+                  color: "rgba(217, 108, 137, 1)",
+                  background:'rgba(242, 208, 215, 1)',
+                  zIndex:2
+                }}
+              >
+                {highlightText.slice(MAX_LENGTH)}
+              </span>
+            )}
+          </span>
+        </div>
 
         <form>
           <textarea
@@ -52,14 +53,14 @@ export const App = () => {
             style={{
               width: "560px",
               height: "264px",
-              border: isOverLimit ? "1px solid #D96C89" : "1px solid #212126",
-              backgroundColor: isOverLimit ? "#FFE6E6" : "#FDFCFC",
+              border: isOverLimit ? "1px solid #D96C89" : "0px solid #212126",
+              backgroundColor: isOverLimit ? "rgba(242, 208, 215, 0.1)" : "#FDFCFC",
             }}
             value={highlightText}
             onChange={handleChange}
           ></textarea>
         </form>
-      </div >
-    </div >
+      </div>
+    </div>
   );
 };
